36e3930debb6cf3ade9b1812437c3ef693220fab
ryry013 <ryry013@yahoo.com>
2021-07-05 11:10:38 -0700

    Draft final commit for FFT task
    - `FFT.cxx` is the macro for running the FFT of the mu2e data
    - `FFT_simple_sine.cxx` is an educational file for running FFT on a simple
    sine wave. This prints out a bunch of information about the FFT into
    `fft_simple_sine_results.xlsx`
    - `TH1_simple_examples.cxx` is some simple gaussian histograms for practice
    making histograms in ROOT
    - In the images file are various images from the different scripts.

--- macros/FFT.cxx
+++ macros/FFT.cxx
@@ -1,58 +1,135 @@
+//
+// take raw data and plot profile
+//
+
+#include <iostream>
+
+R__LOAD_LIBRARY(../libData.so);  // this only works if you have $PROJECT defined in .bashrc
+
+// brackets look for header files in predetermined directory path, used for system header files
+// looks relative to current folder, used for user header files
+
+// TTree is a list of columns/branches called TBranch
+#include <TTree.h> // https://root.cern.ch/doc/master/classTTree.html
+
+// TGraph is a graph class to make graphs by specifying all x and y points
+#include <TGraph.h> // https://root.cern.ch/doc/master/classTGraph.html
+
+// References a ROOT data file
+#include <TFile.h> // https://root.cern.ch/doc/master/classTFile.html
+
+// TSystem interacts with the OS. For us, this is the Load command
+#include <TSystem.h> // https://root.cern.ch/doc/master/classTSystem.html
+
+// Creates a window to put graphs in (we create three)
+#include "TCanvas.h" // https://root.cern.ch/doc/master/classTCanvas.html
+
+// 2D Histograms, displays mean value of Z + error for each cell in
+#include "TProfile2D.h" // https://root.cern.ch/doc/master/classTProfile2D.html
 #include "TH1.h"
 #include "TF1.h"
-#include "TCanvas.h"
 
-void histo() {
 
-	int n_bins = 1283;  // x-axis will be split into this many bins. if too low, FFT won't work well
-  	double x_min = -2.;  // this can be anything
-	double x_max = 9.;  // this can be anything
-	double freq = 23.;  // this is a secret number that we want to get through FFT only
+// Define special styles, editing gStyle pointer edits default style
+#include <TStyle.h> // https://root.cern.ch/doc/master/classTStyle.html
+
+// void means this function does not have a return value
+// first argument is called "rootfile" of type char*
+// second argument is called "nevn" of type int
+// We write `testShowProf_rawevent("../data/1000evn_v3.root",100)`
+
+void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 1000)
+{
+  using std::count;  // lets you use count without having to type std::count everytime
+  using std::endl;  // lets you use std::endl without having to type std::endl everytime
+  gStyle ->SetCanvasDefH(900);  // Arrow sets the SetCanvasDefH variable of the gStyle pointer
+  gStyle ->SetCanvasDefW(1500);  // Sets default width/height of canvases
+
+  gSystem->Load("../libData.so");  // from TSystem.h, loads things like RawEvent
+  gSystem->Load("../dataClasses/DataCint_rdict.pcm");  // for root6 
+
+  TFile *fr = new TFile(rootfile);  // loads the data in the .root file
+
+  TTree *tr = (TTree*)fr->Get("rtree");  // creates a tree to store the data from data "fr"
+
+  RawEvent *revent = new RawEvent();
+
+  Int_t ch_vpeaksum;
+
+  Double_t ch_vpeaksum_time1_us;
+
+  Int_t nsum_ch3; 
+
+  double t_min = 1400.;  // kind of chosen arbitrarily
+  double t_max = 1700.;
+  double nt = 500*(t_max - t_min);
   
-  	TH1F * hist = new TH1F("hist","hist", n_bins, x_min, x_max);  // will create FFT from this
-  	TF1  * func = new TF1("func", "cos(2.0 * TMath::Pi() * [0] * x)", x_min, x_max);  // to create data
-	func->SetParameter(0, freq);  // sets the [0] in the above line to freq
+  TH1F *hist = new TH1F("hist", "Time events histogram;Time (us);Acc. Trig. Events", nt, t_min, t_max);
   
-  	for (int bin = 1; bin <= n_bins; ++bin){
-    		double bin_center = hist -> GetBinCenter(bin);
-    		double func_value = func -> Eval (bin_center);
-    		hist -> SetBinContent(bin, func_value);  // evaluate func at the center of each bin
-  	}
-  
-  	TH1 * hist_transform;  // temporary unscaled object to contain the FFT
-  	hist_transform = hist -> FFT(hist_transform, "MAG");
-	
-	TCanvas *c = new TCanvas("c","c",2000,1000);
-	c->Divide(3,1);
+  tr->SetBranchAddress("ch3.",&revent);  // Relates revent to our .root file data here
+  // Specifically, it chooses to read out data from channel 3 in the root file
 
-	c->cd(1);
-	hist->Draw();  // original function in the time domain
+  for(int i= 0; i< nevn ;i++)
+   {
+   	// starts to read data from the .root file (now imported as a TTree file called tr)
+        tr->GetEntry(i);
+        nsum_ch3 = revent->GetVPeakSumSize();  // number of peaks in this event
 
-	c->cd(2);
-	hist_transform->Draw();  // unscaled FFT result
-	
-	c->cd(3);
-	TH1 * scaled_hist = (TH1*)hist_transform->Clone();  // create a copy of the above to scale
-	double sample_rate = (x_max - x_min)/n_bins;  // only important for the next line
-	double scale_factor = 1/(sample_rate * n_bins);  // how much we'll rescale the x-axis by
-	scaled_hist->SetBins(n_bins, 0, n_bins * scale_factor);  // relabels the x-axis
-	scaled_hist->Draw();
+       for(int j =0; j< nsum_ch3 ;j++)
+       {
 
-	printf("n_bins = %d\n", n_bins);
-	printf("x_range = %f -> %f\n", x_min, x_max);
-	printf("freq = %f\n", freq);
-	printf("sample rate = %f\n", sample_rate);
-	double max_bin = scaled_hist->GetMaximumBin(); 
-	printf("max bin = %f\n", max_bin);
-	printf("Predicted freq = %f\n", scaled_hist->GetXaxis()->GetBinLowEdge(max_bin));
-	
-	double bin_hz = freq/max_bin;
-	printf("Hz per bin = %f\n", bin_hz);
-	
-	double max_hz = (n_bins/2) * bin_hz;
-	printf("Max Hz = %f\n", max_hz);
-	
-	printf("Scale factor = %f\n", scale_factor);
+          ch_vpeaksum_time1_us=revent->GetVPeakSumTime1()[j]*0.001; //convert to us
+
+	  hist->Fill(ch_vpeaksum_time1_us);
+
+       }
+  } 
+
+  TCanvas *c1 = new TCanvas(
+		  "c1",  // name
+		  "profile sum ms ",  // title
+		  200,  // wtopx, pixel x-location of top-left corner of the canvas
+		  10,  // wtopy, pixel y-location of ...
+		  1800,  // width
+		  1000  // height
+		  );
+ 
+  // ////////////// Start making plots ////////////////
+
+  c1->Divide(3,1);
+
+  c1->cd(1);
+  hist->Draw();
+
+  TH1 *unscaled_FFT = 0;  // Temporary object
+  TVirtualFFT::SetTransform(0);  // Not sure what this does
+  unscaled_FFT = hist->FFT(unscaled_FFT, "MAG");  // Do FFT
+  TH1 * final_FFT = (TH1*)unscaled_FFT->Clone();  // Clone the FFT to scale it
+  double sample_rate = (t_max - t_min) / nt; 
+  final_FFT -> SetBins(nt, 0, nt / (sample_rate * nt));  // Scale FFT
+
+  c1->cd(2);
+  unscaled_FFT->SetTitle("Unscaled full FFT result;Unscaled frequency;Magnitude");
+  unscaled_FFT->Draw();
+
+  c1->cd(3);
+  final_FFT->GetXaxis()->SetRange(2,100);  // skip first bin which is just average DC power
+  final_FFT->SetTitle("FFT Result (First 100 Bins);Frequency (MHz);Magnitude");
+
+  final_FFT->Draw();
+
+  printf("First 100 bins of FFT result: \n");
+//  for (int i=0; i < 100; i++) {
+//	  printf("%d: ", i);
+//	  printf("%f\n", final_FFT->GetBinContent(i));
+//  }
+  int max_bin = final_FFT->GetMaximumBin();
+  printf("Max bin: %d\n", max_bin);
+  double max_bin_low_edge = final_FFT->GetXaxis()->GetBinLowEdge(max_bin);
+  printf("Max bin location (freq. in MHz): %f\n", max_bin_low_edge);
+  double period = 1/max_bin_low_edge;
+  printf("Predicted period (us): %f\n", period);
+
+  return 0;
 
 }
-
