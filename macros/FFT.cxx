f2b33a2e54cff03a080e3311dc80a1c6dc8d0928
ryry013 <ryry013@yahoo.com>
2021-07-09 00:01:52 -0700

    Modulated over period of first injection
    See images in macros/images/modulated_* for results.

--- macros/FFT.cxx
+++ macros/FFT.cxx
@@ -33,12 +33,14 @@ R__LOAD_LIBRARY(../libData.so);  // this only works if you have $PROJECT defined
 // Define special styles, editing gStyle pointer edits default style
 #include <TStyle.h> // https://root.cern.ch/doc/master/classTStyle.html
 
+#include <vector>
+
 // void means this function does not have a return value
 // first argument is called "rootfile" of type char*
 // second argument is called "nevn" of type int
 // We write `testShowProf_rawevent("../data/1000evn_v3.root",100)`
 
-void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 1000)
+void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 1)
 {
 	using std::count;  // lets you use count without having to type std::count everytime
 	using std::endl;  // lets you use std::endl without having to type std::endl everytime
@@ -63,12 +65,13 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 	double t_min = 0.;  
 	double t_max = 3500.;
 	double nt = 5*(t_max - t_min);
+	
+	vector<double> time_vector;
 
 	TH1F *hist = new TH1F("hist", "Time events histogram;Time (us);Acc. Trig. Events", nt, t_min, t_max);
 
 	tr->SetBranchAddress("ch3.",&revent);  // Relates revent to our .root file data here
 	// Specifically, it chooses to read out data from channel 3 in the root file
-
 
 
 	for(int i= 0; i< nevn ;i++) {
@@ -79,12 +82,15 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 		for(int j =0; j< nsum_ch3 ;j++)
 		{
 			event_time = revent->GetVPeakSumTime1()[j]*0.001; //convert to us
-			hist->Fill(event_time);
+			if (t_min < event_time && event_time < t_max) {
+				time_vector.push_back(event_time);
+				hist->Fill(event_time);
+			}
 		}
 	}
 		
 	/* 	
-	
+
 	int peak1 = 0;
 	int peak2 = 0;
 	int peak3 = 0;
@@ -93,25 +99,25 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 
 	Int_t bin_number;
 	Double_t bin_content;  
-	
+
 	if (0 < event_time && event_time < 3000) {
 				hist->Fill(event_time);
 			}
 					
-   			else if (067000 < event_time && event_time < 070000) {
-   				hist->Fill(event_time - 67000 + 3000);
-   			}
-   			
-   			else if (267000 < event_time && event_time < 270000) {
-   				hist->Fill(event_time - 267000 + 6000);
-   			}
-   			else if (334000 < event_time && event_time < 337000) {
-   				hist->Fill(event_time - 334000 + 9000);
-   			} 
-   			else if (534000 < event_time && event_time < 537000) {
-   				hist->Fill(event_time - 534000 + 12000);
-   			}
-	
+			else if (067000 < event_time && event_time < 070000) {
+				hist->Fill(event_time - 67000 + 3000);
+			}
+			
+			else if (267000 < event_time && event_time < 270000) {
+				hist->Fill(event_time - 267000 + 6000);
+			}
+			else if (334000 < event_time && event_time < 337000) {
+				hist->Fill(event_time - 334000 + 9000);
+			} 
+			else if (534000 < event_time && event_time < 537000) {
+				hist->Fill(event_time - 534000 + 12000);
+			}
+
 	for (int bin = 0; bin <= nt; bin++) {
 		time = hist->GetBinLowEdge(bin);
 		if (bin_content > 150) {
@@ -124,64 +130,106 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 	bin_content = hist->GetBinContent(bin_number);
 
 	if (bin_content > 150.) {
-	       printf("Bin number: %d\t", bin_number);
-	       printf("Bin time: %f\t", event_time);
-	       printf("Bin content: %f\n", bin_content);
+		   printf("Bin number: %d\t", bin_number);
+		   printf("Bin time: %f\t", event_time);
+		   printf("Bin content: %f\n", bin_content);
 	}
 
 	// if (0 < event_time < 3000) {
 	//        Double_t peak1 = hist->GetXaxis()->event_time
-	       hist->Fill(event_time);
+		   hist->Fill(event_time);
 	// }
 	// else if (
 
 	*/
 
-  TCanvas *c1 = new TCanvas(
-		  "c1",  // name
-		  "profile sum ms ",  // title
-		  200,  // wtopx, pixel x-location of top-left corner of the canvas
-		  10,  // wtopy, pixel y-location of ...
-		  1800,  // width
-		  1000  // height
-		  );
+	TCanvas *c1 = new TCanvas(
+		"c1",  // name
+		"Fourier Transform",  // title
+		200,  // wtopx, pixel x-location of top-left corner of the canvas
+		10,  // wtopy, pixel y-location of ...
+		1800,  // width
+		1000  // height
+	);
  
-  // ////////////// Start making plots ////////////////
+	// ////////////// Start making plots ////////////////
 
-  c1->Divide(3,1);
+	c1->Divide(3,1);
 
-  c1->cd(1);
-  hist->Draw();
+	c1->cd(1);
+	hist->Draw();
 
-  TH1 *unscaled_FFT = 0;  // Temporary object
-  TVirtualFFT::SetTransform(0);  // Not sure what this does
-  unscaled_FFT = hist->FFT(unscaled_FFT, "MAG");  // Do FFT
-  TH1 * final_FFT = (TH1*)unscaled_FFT->Clone();  // Clone the FFT to scale it
-  double sample_rate = (t_max - t_min) / nt; 
-  final_FFT -> SetBins(nt, 0, nt / (sample_rate * nt));  // Scale FFT
+	TH1 *unscaled_FFT = 0;  // Temporary object
+	TVirtualFFT::SetTransform(0);  // Not sure what this does
+	unscaled_FFT = hist->FFT(unscaled_FFT, "MAG");  // Do FFT
+	TH1 * final_FFT = (TH1*)unscaled_FFT->Clone();  // Clone the FFT to scale it
+	double sample_rate = (t_max - t_min) / nt; 
+	final_FFT -> SetBins(nt, 0, nt / (sample_rate * nt));  // Scale FFT
 
-  c1->cd(2);
-  unscaled_FFT->SetTitle("Unscaled full FFT result;Unscaled frequency;Magnitude");
-  unscaled_FFT->Draw();
+	c1->cd(2);
+	unscaled_FFT->SetTitle("Unscaled full FFT result;Unscaled frequency;Magnitude");
+	unscaled_FFT->Draw();
 
-  c1->cd(3);
-  final_FFT->GetXaxis()->SetRange(10,1000);  // skip first bin which is just average DC power
-  final_FFT->SetTitle("FFT Result (First 1000 Bins);Frequency (MHz);Magnitude");
+	c1->cd(3);
+	final_FFT->GetXaxis()->SetRange(10,1000);  // skip first bin which is just average DC power
+	final_FFT->SetTitle("FFT Result (First 1000 Bins);Frequency (MHz);Magnitude");
 
-  final_FFT->Draw();
+	final_FFT->Draw();
 
-  printf("First 100 bins of FFT result: \n");
-//  for (int i=0; i < 100; i++) {
-//	  printf("%d: ", i);
-//	  printf("%f\n", final_FFT->GetBinContent(i));
-//  }
-  int max_bin = final_FFT->GetMaximumBin();
-  printf("Max bin: %d\n", max_bin);
-  double max_bin_low_edge = final_FFT->GetXaxis()->GetBinLowEdge(max_bin);
-  printf("Max bin location (freq. in MHz): %f\n", max_bin_low_edge);
-  double period = 1/max_bin_low_edge;
-  printf("Predicted period (us): %f\n", period);
-
-  return 0;
+	printf("First 1000 bins of FFT result: \n");
+	//  for (int i=0; i < 100; i++) {
+	//	  printf("%d: ", i);
+	//	  printf("%f\n", final_FFT->GetBinContent(i));
+	//  }
+	int max_bin = final_FFT->GetMaximumBin();
+	printf("Max bin: %d\n", max_bin);
+	double max_bin_low_edge = final_FFT->GetXaxis()->GetBinLowEdge(max_bin);
+	printf("Max bin location (freq. in MHz): %f\n", max_bin_low_edge);
+	double period = 1/max_bin_low_edge;
+	printf("Predicted period (us): %f\n", period);
+	
+	t_min = 5;  
+	t_max = 9;
+	nt = 200*(t_max - t_min);
+	
+	TH1F *zoom_hist = new TH1F("zoom_hist", "Modulated Period Histogram;Time (us);Acc. Trig. Events", nt, t_min, t_max);
+	
+	for (int i = 0; i < time_vector.size(); i++) {
+		double mod_result = fmod(time_vector[i], period);
+		if (mod_result <= 6) {
+			zoom_hist->Fill(mod_result + 6.);
+		}
+		else if (6 < mod_result) {
+			zoom_hist->Fill(mod_result - (period - 6) );
+		}
+	}
+  
+	TCanvas *c2 = new TCanvas(
+		"c2",  // name
+		"Fine Peak Detail",  // title
+		200,  // wtopx, pixel x-location of top-left corner of the canvas
+		10,  // wtopy, pixel y-location of ...
+		1800,  // width
+		1000  // height
+		);
+		
+	c2->Divide(2, 1); 
+	
+	c2->cd(1);
+	zoom_hist->Draw();
+	
+	TH1 *unscaled_modulated_FFT = 0;  // Temporary object
+	TVirtualFFT::SetTransform(0);  // Not sure what this does
+	unscaled_modulated_FFT = zoom_hist->FFT(unscaled_modulated_FFT, "MAG");  // Do FFT
+	TH1 * final_modulated_FFT = (TH1*)unscaled_modulated_FFT->Clone();  // Clone the FFT to scale it
+	sample_rate = (t_max - t_min) / nt; 
+	final_modulated_FFT -> SetBins(nt, 0, nt / (sample_rate * nt));  // Scale FFT
+	
+	final_modulated_FFT->GetXaxis()->SetRange(10,nt/2);  // skip first bin which is just average DC power
+	
+	c2->cd(2);
+	final_modulated_FFT->Draw();
+	
+	return 0;
 
 }
