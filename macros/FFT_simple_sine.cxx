fd371aff2fb3a4c245c709ccff1ae3787bdc7a2a
ryry013 <ryry013@yahoo.com>
2021-07-15 13:22:26 -0700

    Now able to set freq. res. in sine example file
    In the FFT_simple_sine file, rather than how it was before where you set
    x_min, x_max, and n_bins, now you set your desired frequency resolution
    and max frequency (nyquist frequency), and it sets x_max and n_bins
    based on that.

--- macros/FFT_simple_sine.cxx
+++ macros/FFT_simple_sine.cxx
@@ -3,23 +3,32 @@
 #include "TCanvas.h"
 
 void histo() {
+	double freq_res = 0.0001;
+	double nyquist_freq = 50;
+	double sample_rate = 1/(nyquist_freq * 2.);
 
-	int n_bins = 1283;  // x-axis will be split into this many bins. if too low, FFT won't work well
-  	double x_min = -2.;  // this can be anything
-	double x_max = 9.;  // this can be anything
-	double freq = 23.;  // this is a secret number that we want to get through FFT only
-  
+  	double x_min = 0.;
+	double x_max = 1/freq_res;  
+	//double x_max = 10;
+	//int n_bins = 100;
+	int n_bins = x_max/sample_rate;  // x-axis will be split into this many bins. if too low, FFT won't work well
+
+	printf("%f\t%d\n", x_max, n_bins);
+	
+	double freq = 23.123456;  // this is a secret number that we want to get through FFT only
+
   	TH1F * hist = new TH1F("hist","hist", n_bins, x_min, x_max);  // will create FFT from this
   	TF1  * func = new TF1("func", "cos(2.0 * TMath::Pi() * [0] * x)", x_min, x_max);  // to create data
 	func->SetParameter(0, freq);  // sets the [0] in the above line to freq
   
   	for (int bin = 1; bin <= n_bins; ++bin){
-    		double bin_center = hist -> GetBinCenter(bin);
+    		double bin_center = hist -> GetBinLowEdge(bin);
     		double func_value = func -> Eval (bin_center);
     		hist -> SetBinContent(bin, func_value);  // evaluate func at the center of each bin
   	}
-  
-  	TH1 * hist_transform;  // temporary unscaled object to contain the FFT
+
+	
+  	TH1 * hist_transform = 0;  // temporary unscaled object to contain the FFT
   	hist_transform = hist -> FFT(hist_transform, "MAG");
 	
 	TCanvas *c = new TCanvas("c","c",2000,1000);
@@ -33,26 +42,29 @@ void histo() {
 	
 	c->cd(3);
 	TH1 * scaled_hist = (TH1*)hist_transform->Clone();  // create a copy of the above to scale
-	double sample_rate = (x_max - x_min)/n_bins;  // only important for the next line
 	double scale_factor = 1/(sample_rate * n_bins);  // how much we'll rescale the x-axis by
 	scaled_hist->SetBins(n_bins, 0, n_bins * scale_factor);  // relabels the x-axis
 	scaled_hist->Draw();
 
-	printf("n_bins = %d\n", n_bins);
-	printf("x_range = %f -> %f\n", x_min, x_max);
-	printf("freq = %f\n", freq);
-	printf("sample rate = %f\n", sample_rate);
-	double max_bin = scaled_hist->GetMaximumBin(); 
-	printf("max bin = %f\n", max_bin);
-	printf("Predicted freq = %f\n", scaled_hist->GetXaxis()->GetBinLowEdge(max_bin));
+	printf("%d\t", n_bins);
+	printf("%f\t", x_min);
+	printf("%f\t", x_max);
+	printf("%f\t", freq);
+	printf("%f\t", sample_rate);
 	
-	double bin_hz = freq/max_bin;
-	printf("Hz per bin = %f\n", bin_hz);
+	double max_bin = scaled_hist->GetMaximumBin(); 
+	printf("%f\t", max_bin);
+	double pred_freq = scaled_hist->GetXaxis()->GetBinLowEdge(max_bin);
+	printf("%f\t", pred_freq);
+	
+	double bin_hz = pred_freq/(max_bin-1);
+	printf("%f\t", bin_hz);
 	
 	double max_hz = (n_bins/2) * bin_hz;
-	printf("Max Hz = %f\n", max_hz);
+	printf("%f\t", max_hz);
 	
-	printf("Scale factor = %f\n", scale_factor);
-
+	printf("%f\n", scale_factor);
+	
+	return 0;
 }
 
