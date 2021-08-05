ae67ebad5c52282955e8b03a262635800291b790
ryry013 <ryry013@yahoo.com>
2021-08-05 12:54:43 -0700

    RawEvent analysis images

--- macros/rawevent_analysis.cxx
+++ macros/rawevent_analysis.cxx
@@ -16,7 +16,7 @@ R__LOAD_LIBRARY(../libData.so);
 #include <vector>
 
 
-void f(char const *rootfile = "../data/1000evn_v3.root", int nevn = 15)
+void f(char const *rootfile = "../data/1000evn_v3.root", int nevn = 1000)
 {
 	gSystem->Load("../libData.so");  // from TSystem.h, loads things like RawEvent
 	gSystem->Load("../dataClasses/DataCint_rdict.pcm");  // for root6 
@@ -28,39 +28,25 @@ void f(char const *rootfile = "../data/1000evn_v3.root", int nevn = 15)
 	RawEvent *revent = new RawEvent();
 	
 	Double_t nsum;
-	Double_t event_time; 
-	Double_t event_time_us; 
-	vector<Double_t> time_vector;
+	Double_t time0; Double_t time1; Double_t time2;
+	Double_t area;
+	Double_t height;
+	// vector<Double_t> time_vector;
 	
 	tr->SetBranchAddress("ch3.",&revent);  
 	
-	// tr->GetEntry(0);
+	int t_min = 0;
+	int t_max = 200;
+	int nt = (t_max - t_min) / 4;
 	
-	// nsum = revent -> GetVPeakSumSize();
-	// printf("GetVPeakSumSize(): %f\n", nsum); 
-	
-	// TBranch * br = tr->GetBranch("ch3.fvPeakSumTime1");
-	
-	// printf("Manual attainment of vector\n");
-	
-	// const Double_t * event_time_2 = revent -> GetVPeakSumTime1();
-	
-	/*
-	printf("GetVPeakSumTime1: %f\n", *event_time_2);
-	printf("GetVPeakSumTime1: %f\n", revent -> GetVPeakSumTime1()[1]);
-	printf("GetVPeakSumTime1: %f\n", revent -> GetVPeakSumTime1()[2]);
-	printf("GetVPeakSumTime1: %f\n", revent -> GetVPeakSumTime1()[3]);
-	printf("GetVPeakSumTime1: %f\n", revent -> GetVPeakSumTime1()[4]);
-	*/
-	
-	// Double_t size = nsum_vector.size();
-	// printf("Size of GetVPeakSum(): %f\n", size);
+	TH1F * hit_length = new TH1F("hit_length", "Full length of hit;Time (ns);Amplitude", nt, t_min, t_max); 
+	TH1F * peak_location = new TH1F("peak_location", "Time to max peak;Time (ns);Amplitude",13 , 0, 52); 
+	TH1F * peak_area = new TH1F("peak_area", "Peak area;Area;Amplitude", 3000, 0, 150000);
+	TH1F * peak_high = new TH1F("GetVPeakHigh()", "Magnitude peak voltage;Voltage;Amplitude", 500, 0, 2100); 
 
 
 	// ////////////// Pull data out of .ROOT file ///////////////
 	
-
-	int count = 0;
 	
 	for(int i= 0; i< nevn; i++) {
 		tr->GetEntry(i);
@@ -75,16 +61,26 @@ void f(char const *rootfile = "../data/1000evn_v3.root", int nevn = 15)
 		printf("GetVPeakSum0_peakSize(): %f\n\n", revent -> GetVPeakSum0_peakSize());
 		*/
 
-		for(int j =0; j < 3; j++)
+		for(int j =0; j < nsum; j++)
 		{	
-			count++;
-			// event_time = revent -> GetVPeakSumTime1();
+			//time0 = revent -> GetVPeakSumTime0()[j];
+			//time1 = revent -> GetVPeakSumTime1()[j];
+			//time2 = revent -> GetVPeakSumTime2()[j];
 			
+			// area = revent -> GetVPeakSum()[j];
+			
+			height = revent -> GetVPeakHigh()[j];
+			
+			//hit_length -> Fill(time2 - time1);
+			//peak_location -> Fill(time0 - time1);
+			// peak_area -> Fill(area * -1);
+			peak_high -> Fill(height * -1);
 			
 			// printf("GetVAmp(): %f\n", revent -> GetVAmp()[j]);
 			// printf("GetVTime(): %f\n", revent -> GetVTime()[j]);
 			// printf("GetVPeakSum_tail(): %f\n", revent -> GetVPeakSum_tail()[j]);
 			
+			/*
 			printf("GetVPeakSum0_peak(): %f\n", revent -> GetVPeakSum0_peak()[j]);
 			printf("GetVPeakHigh(): %f\n", revent -> GetVPeakHigh()[j]);
 			printf("GetVPeakSum0(): %f\n", revent -> GetVPeakSum0()[j]);
@@ -94,6 +90,7 @@ void f(char const *rootfile = "../data/1000evn_v3.root", int nevn = 15)
 			printf("GetVPeakHighTime(): %f\n", revent -> GetVPeakHighTime()[j]);
 			printf("GetVPeakSumTime0(): %f\n", revent -> GetVPeakSumTime0()[j]);
 			printf("GetVPeakSumTime2(): %f\n\n", revent -> GetVPeakSumTime2()[j]);
+			*/
 			
 			// event_time = revent -> GetVPeakSumTime1()[j]; // in ns
 			// printf("event_time 1: %f\n", event_time);
@@ -102,10 +99,50 @@ void f(char const *rootfile = "../data/1000evn_v3.root", int nevn = 15)
 		}
 		
 	}
+	/*
+	TCanvas *c1 = new TCanvas(
+		"c1",  // name
+		"Peak Lengths",  // title
+		200,  // wtopx, pixel x-location of top-left corner of the canvas
+		10,  // wtopy, pixel y-location of ...
+		1800,  // width
+		1000  // height
+	);
 	
-	printf("Final count value: %d\n", count);
-
-	// printf("time_vector.size(): %zu\n", time_vector.size());
+	c1 -> Divide(2,1);
+	
+	c1 -> cd(1);
+	hit_length -> Draw();
+	
+	c1 -> cd(2);
+	peak_location -> Draw();
+	*/
+	
+	/*
+	TCanvas *c2 = new TCanvas(
+		"c2",
+		"Peak Area",
+		200,
+		10,
+		800,
+		800
+	);
+	
+	c2 -> cd();
+	peak_area -> Draw();
+	*/
+	
+	TCanvas *c3 = new TCanvas(
+		"c3",
+		"Peak Height",
+		200,
+		10,
+		800,
+		800
+	);
+	
+	c3 -> cd();
+	peak_high -> Draw();
 	
 	return 0;
 
