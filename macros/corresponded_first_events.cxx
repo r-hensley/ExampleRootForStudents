062a4b15ea137933237ed14b715f5a78e5f9bf04
ryry013 <ryry013@yahoo.com>
2021-08-13 15:16:43 -0700

    Hard-set correct period graphs

--- macros/corresponded_first_events.cxx
+++ macros/corresponded_first_events.cxx
@@ -11,7 +11,9 @@ R__LOAD_LIBRARY(../libData.so)
 #include <TStyle.h>
 #include "TH1.h"
 #include "TF1.h"
+#include "TH2.h"
 #include <vector>
+#include <TProfile2D.h> 
 
 //HISTOGRAMS
 void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 1000)
@@ -37,6 +39,13 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 	vector<Double_t> time_vector;
 	
 	Int_t nsum_ch3;
+	
+	// first peak: 68.8 - 69.5
+	// second peak: 79.9 - 80.6
+	// third peak: 90.8 - 91.6
+	// fourth peak: 102.3 - 102.8
+	Double_t lower_threshhold = 0.;
+	Double_t upper_threshhold = 1000.;
 
 	double freq_res = 0.0001;  // Width of bins in MHz on final FFT 
 	double nyquist_freq = 0.12;
@@ -57,6 +66,10 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 									nt, t_min, t_max);
 					
     hit_difference -> GetXaxis() -> SetRangeUser(0, 5000);
+    
+    TH2F *modulated_profile = new TH2F("modulated_profile", "All events, all  hits;Modulated time (us); Event time (ms)", 
+    100, -11.134488/2, 11.134488/2, 
+    50, 0, 550);
 
 	tr->SetBranchAddress("ch3.", &revent);
 	
@@ -76,13 +89,10 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 			if (first_event_time == 0) {
 				first_event_time = event_time;
 				
-				// first peak: 68.8 - 69.5
-				// second peak: 79.9 - 80.6
-				// third peak: 90.8 - 91.6
-				// fourth peak: 102.3 - 102.8
-				if (first_event_time < 25. || 5000. < first_event_time) {
+
+				if (first_event_time < lower_threshhold || upper_threshhold < first_event_time) {
 					// ignore all events with the first hit not being in this range
-					break;
+					// break;
 				}
 				else { original->Fill(event_time); }
 			}
@@ -90,33 +100,44 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 			else {
 				original->Fill(event_time);
 				difference = event_time - first_event_time;
+				
+				if (difference > 1000) {first_event_time = event_time; difference = event_time - first_event_time;}
+				
 				hit_difference -> Fill(difference);
 				time_vector.push_back(difference);
+				
+				Double_t mod_result = fmod(difference, 11.134488);
+				// if (mod_result > 1) {printf("%f\t%f\t%f\n", first_event_time, difference, mod_result);}
+				if (mod_result <= 11.134488 / 2) { modulated_profile -> Fill( mod_result, (difference + first_event_time) / 1000 ); }
+				else if (11.134488 / 2 < mod_result) { modulated_profile -> Fill( mod_result - 11.134488, (difference + first_event_time) / 1000); }
 				}
 			}
 
 		}
+		
+		
+	
+	// ~~~~~~~~~~~~~ Do FFT ~~~~~~~~~~~~~~~~~~ //
+	
 	
 	TH1 *unscaled_FFT = 0;  // Temporary object
 
 	TVirtualFFT::SetTransform(0);  // Not sure what this does
-	unscaled_FFT = original -> FFT(unscaled_FFT, "MAG");  // Do FFT
+	unscaled_FFT = hit_difference -> FFT(unscaled_FFT, "MAG");  // Do FFT
 	TH1 * final_FFT = (TH1*)unscaled_FFT->Clone();  // Clone the FFT to scale it
 	final_FFT -> SetBins(nt, 0, nt / (sample_rate * nt));  // Scale FFT
     
-	TCanvas *c1 = new TCanvas("c1", "Hit Difference", 200, 10, 1700, 900);
-    
-	TCanvas *c2 = new TCanvas("c2", "Modulo histogram", 200, 10, 1700, 900);
+	final_FFT -> GetXaxis() -> SetRange(50, nt / 2);  // Last  half of FFT is negative frequencies
 	
-	// Set range of FFT graph for calculation of max bin / period	
-	final_FFT -> GetXaxis() -> SetRange(50, nt / 2);
+	// ~~~~~~~~~~~~ Calculate period ~~~~~~~~~~~~~ //
 
-	int max_bin = final_FFT->GetMaximumBin();
+	int max_bin = final_FFT -> GetMaximumBin();  // get max bin number
 	
 	Double_t max_bin_content = final_FFT -> GetBinContent(max_bin);
 	Double_t before_bin_content = final_FFT -> GetBinContent(max_bin - 1);
 	Double_t after_bin_content = final_FFT -> GetBinContent(max_bin + 1);
 
+	// Interpolation algorithm http://www.add.ece.ufl.edu/4511/references/ImprovingFFTResoltuion.pdf
 	Double_t bin_shift = (log(after_bin_content/before_bin_content))/(2*log((max_bin_content * max_bin_content)/(before_bin_content * after_bin_content)));
 	
 	Double_t new_bin = max_bin + bin_shift;
@@ -125,12 +146,17 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 	Double_t max_bin_low_edge = final_FFT->GetXaxis()->GetBinLowEdge(max_bin);
 
 	Double_t period = 1/freq;
-	// period = 11.1345;
 
 	printf("Max bin: %d\n", max_bin);
 	printf("Max bin location (freq. in MHz): %f\n", max_bin_low_edge);
 	printf("Predicted frequency (MHz): %f\n", freq);
 	printf("Predicted period (us): %f\n", period);
+	
+	period = 11.134488;
+	
+	// ~~~~~~ Plot first canvas: hit difference and FFT ~~~~~~~ //
+	
+	TCanvas *c1 = new TCanvas("c1", "Hit Difference", 200, 10, 1700, 900);
 
 	c1->Divide(2,1);
 
@@ -141,39 +167,33 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
     c1->cd(2);
     final_FFT->Draw();
     final_FFT->SetFillColor(30);
+    
+    // ~~~~~~~~~~~~~ Create modulated histograms ~~~~~~~~~~~~~~~~~ //
+    
+
 	
 	int time_per_bin_ns = 5; // ns per bin
 	Double_t time_per_bin_us = time_per_bin_ns / 1000.; 
 	
-	t_min = 0;  
-	t_max = period;
-	nt = t_max / time_per_bin_us;
+	t_min = -period / 2;  
+	t_max = period / 2;
+	nt = (t_max - t_min) / time_per_bin_us;
 	
 	TH1F *zoom_hist = new TH1F(
 	"zoom_hist", 
 	"Period-Modulated Histogram;Time (us);Acc. Trig. Events", 
 	nt, t_min, t_max);
 	
-	double split = 0.;  // split is only for if the modulo splits the histogram in half
+	// shift period/2 <--> period to -period/2 <--> 0
 	for (int i = 0; i < time_vector.size(); i++) { 
 		Double_t mod_result = fmod(time_vector[i], period);  // modulo
-		if (split == 0.) {
-			// printf("time: %f\tfinal result: %f\n", time_vector[i], mod_result);
-			zoom_hist->Fill(mod_result);
-		}
-		
-		// if split = 0, ignore below
-		else if (mod_result <= split) {
-			Double_t to_fill = fmod(mod_result + split, period);
-			printf("time: %f\tmod_result: %f\tfinal result: %f\n",time_vector[i], mod_result, to_fill );
-			zoom_hist->Fill( to_fill );
-		}
-		else if (split < mod_result) {
-			Double_t to_fill = fmod(mod_result + period + split, period);
-			printf("time: %f\tmod_result: %f\tfinal result: %f\n",time_vector[i], mod_result, to_fill );
-			zoom_hist->Fill( to_fill );
-		}
+		if (mod_result <= period / 2) { zoom_hist -> Fill( mod_result ); }
+		else if (period / 2 < mod_result) { zoom_hist -> Fill( mod_result - period ); }
 	}
+	
+	// ~~~~~~~~~~~~~~~~~ Plot modulated histograms ~~~~~~~~~~~~~~~~~ //
+	
+	TCanvas *c2 = new TCanvas("c2", "Modulo histogram", 0, 0, 1700, 900);
     
     c2->Divide(3, 1); 
 	
@@ -182,21 +202,28 @@ void signal_FFT(char const *rootfile = "../data/1000evn_v3.root", int nevn = 100
 	
 	c2->cd(2);
 	TH1F * zoom_hist2 = (TH1F*)zoom_hist->Clone();
-	zoom_hist2 -> GetXaxis() -> SetRangeUser(2.25, 6.55);
+	zoom_hist2 -> GetXaxis() -> SetRangeUser(4, 4.5);
 	std::string period_text = "Period: " + std::to_string(period) + " us";	
 	zoom_hist2 -> SetTitle(period_text.c_str());
 	zoom_hist2 -> Draw();
 	
 	c2->cd(3);
 	TH1F * zoom_hist3 = (TH1F*)zoom_hist -> Clone();
-	zoom_hist3 -> GetXaxis() -> SetRangeUser(4.1, 4.25);
-	std::string time_per_bin_text = "Time per bin: " + std::to_string(time_per_bin_ns) + " ns";
-	zoom_hist3 -> SetTitle(time_per_bin_text.c_str());
+	zoom_hist3 -> GetXaxis() -> SetRangeUser(-0.5, 0.5);
+	std::string first_hit_threshhold = "First hit threshhold: " + std::to_string(lower_threshhold).substr(0, std::to_string(lower_threshhold).find(".") + 2) + " - " + std::to_string(upper_threshhold).substr(0, std::to_string(upper_threshhold).find(".") + 2) + " us";
+	zoom_hist3 -> SetTitle(first_hit_threshhold.c_str()); + " ns";
+	zoom_hist3 -> SetTitle(first_hit_threshhold.c_str());
 	zoom_hist3 -> Draw();
     
     zoom_hist3 -> Draw();
     
+    // ~~~~~~~~~~~~~ Plot TProfile2D ~~~~~~~~~~~~~ //
     
+    TCanvas *c3 = new TCanvas("c3", "Modulated Profile", 0, 0, 800, 800);
     
+    c3 -> cd();
+    
+    modulated_profile -> Draw("colz");
+       
     return 0;
 }
